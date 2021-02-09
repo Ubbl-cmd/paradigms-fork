dacfc4fbf5048decd0b59ed9fa406991cc3602ce
Aleksej <313012@niuitmo.ru>
2021-02-09 21:48:42 +0300

    (. , .)

--- lec3/Парадигмы программирования _ Введение в ООП (1)_files/lang-javaModule.js
+++
@@ -1,4 +0,0 @@
-PR['registerLangHandler'](PR['sourceDecorator']({
-    'keywords': 'module,requires,transitive,static,exports,to,opens,uses,provides,with,open',
-    'cStyleComments': true
-}), ['javaModule']);
